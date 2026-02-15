afdd85d4e3adfbb79a625c33b061f0f3bfa34f44
marlonporres <140191539+marlonporres@users.noreply.github.com>
2026-02-15 20:18:56 +0000

    Agregando ejercicios de la clase del sábado

---
+++ Primer ejercicio en clase.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero_1, numero_2, resultado;
+
+    cout << "Hola mundo" << endl;
+
+    cout << "ingrese el número 1: ";
+    cin >> numero_1;
+
+    cout << "Ingrese el número 2: ";
+    cin >> numero_2;
+
+    resultado = numero_1 + numero_2;
+    
+    cout << "El resultado es igual a: " << resultado << endl;
+
+    cout << "Bienvenidos a programación 1\n" << endl;
+    cout << "\tNombre:\tMarlon" << endl;
+    cout << "\tEdad:\t29" << endl;
+    cout << "\tCurso:\tC++\n" << endl;
+
+    cout << "Ejemplo de comillas: \"C++ es genial\"\n";
+    cout << "Ruta de ejemplo: C:\\usuarios\\workspace\\nombre.cpp";
+
+    return 0;
+
+
+}
